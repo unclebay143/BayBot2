026a727e2be8720e1ee82390d121ced67032c847
unclebay143 <ayodelesamueladebayo143@gmail.com>
2020-04-30 10:26:52 -0700

    Added Database Storage Feature
    BayBot can not store information into the data and also lnform about:
    1. Visitors who ask for contacts
    2. Visitor who checked to see you
    
    the local storage (Notepad) data layout has been well developed and arranged.

--- baybot.py
+++ baybot.py
@@ -3,18 +3,29 @@
 # A program that interacts with the users
 # d im user_talk,username,usermessage,confirm,baytalk
 """converting BASIC to PYTHON"""
-import sys, time
-
+import sys
+import time
+import datetime
+import MySQLdb
 developer = "Ayodele Samuel Adebayo"
 developernickname = "UncleBigBay"
+bot_name = "BAY-BOT"
 print(developer + "\n")
 confirm = " "
 name = " "
 
+# CONNECTION TO baybotdb
+db_conn = MySQLdb.connect(
+    host="localhost",
+    db="baybotdb",
+    user="root",
+    password=""
+)
+
 
 # Function for name/greeting animation
 def animation(k):
-    greet = ("\n Welcome " + name.upper() + "," + "\n\n what can I do for you?. (1 at a time?)\n")
+    greet = ("\n Welcome " + k.upper() + "," + "\n\n what can I do for you?. (1 at a time?)\n")
     for i in range(len(greet)):
         time.sleep(0.1)
         sys.stdout.write("" + greet[i % len(greet)])
@@ -23,7 +34,34 @@ def animation(k):
 
 # Function that Gives My number
 def menu2():
-    print("\n07014525696")
+    print("\n07014525696 or 07087661935")
+    bot_message = name.upper() + " took your number, did you received a call?"
+    # saving message to notepad
+    offlinemessage = open("offlinemessage.txt", "a")
+    message_time = datetime.datetime.now()
+    sendmakeup = ("\n\n\n" + "Name: " + bot_name + "\n" + "Message: " +
+                  bot_message + "\n" + "Time: " + str(message_time.strftime("%c")))
+    offlinemessage.write(sendmakeup)
+    offlinemessage.close()
+
+    # saving message to baybotdb
+    cursor = db_conn.cursor()
+    add_visitor_entry = ("INSERT INTO visitor"
+                         "(visitor_name, message)"
+                         "VALUES(%s, %s)")
+    visitor_data_entry = (bot_name, bot_message)
+    cursor.execute(add_visitor_entry, visitor_data_entry)
+    db_conn.commit()
+    cursor.close()
+    db_conn.close()
+
+    menu()
+
+
+# Emergency number
+def emergency():
+    print("\nFor Emergency: Bay's Mom: 08038157444")
+    menu()
 
 
 # Menu3
@@ -34,19 +72,32 @@ def menu3():
 
 # Function for offline message from visitor stored in a txt file
 def menu4():
-    offlinemessage = open("offlinemessage.txt", "w")
+    # saving message to notepad
+    offlinemessage = open("offlinemessage.txt", "a")
+    message_time = datetime.datetime.now()
     writemessage = (input("leave your message: "))
-    sendmakeup = ("Name: " + name + "\n" + "Message: " + writemessage)
+    sendmakeup = ("\n\n\n" + "Name: " + name + "\n" + "Message: " +
+                  writemessage + "\n" + "Time: " + str(message_time.strftime("%c")))
     offlinemessage.write(sendmakeup)
     offlinemessage.close()
-    process = (" \nMessage Sent Successfully")
+    process = " \nMessage Sent Successfully"
     for i in range(len(process)):
         time.sleep(0.1)
         sys.stdout.write("" + process[i % len(process)])
         sys.stdout.flush()
 
+    # saving message to baybotdb
+    cursor = db_conn.cursor()
+    add_visitor_entry = ("INSERT INTO visitor"
+                         "(visitor_name, message)"
+                         "VALUES(%s, %s)")
+    visitor_data_entry = (name, writemessage)
+    cursor.execute(add_visitor_entry, visitor_data_entry)
+    db_conn.commit()
+    cursor.close()
+    db_conn.close()
     returntomenu = "1"
-    ask = input("\n Press 1 to return to menu")
+    ask = input("\n Press 1 to return to menu: ")
     if ask == returntomenu:
         menu()
     else:
@@ -56,7 +107,7 @@ def menu4():
 
 # Def for Area Of A circle
 def areaofacircle():
-    radius = input(" Enter your radius to calculate the area:r ")
+    radius = input(" Enter your radius to calculate the area:r = ")
     r = int(radius)
     r2 = int(r * r)
     pi = 3.143
@@ -67,9 +118,10 @@ def areaofacircle():
     radius = str(radius)
     r = str(r)
     explanation = (
-                "\n\nThe formular for area of a circle is πr^2 \n\n We all know that π is equal to 22%7 = " + pi + " and your radius is " + r + " : - " + pi + " X" + radius + " = " + answer)
-    print
-    print
+            "\n\nThe formular for area of a circle is πr^2 \n\n We Should all know that π is equal to 22%7 = " + pi +
+            " and your radius is " + r + " : - " + pi + " X" + radius + " = " + answer)
+    print()
+    print()
     print("\nShould I explain?.... Y/N")
     explain = input("\n")
     if explain.lower() == "y" or "yes":
@@ -86,27 +138,25 @@ def areaofacircle():
 
 # Function to check user choice of math topic
 def mathtopic(t):
-    global topic
-    if topic != 1:
+    if t == 1:
         areaofacircle()
     else:
-        print("coming soon")
+        print("coming soon!!!")
 
 
 # Mathematics Function # MENU 5
 def menu5():
-    print(" \n welcome, What can i solve for you?")
+    print("\nwelcome, What can i solve for you?")
     print(" 1. Area of a circle or About circle")
     print(" 2. Simple Addition")
     print(" 3. Meaning of BODMAS")
-    global topic
     topic = input("Select: ")
     mathtopic(topic)
 
 
 # function for user request from the menu
 def userrequest(n):
-    if n < 1 or n > 5:
+    if n < 1 or n > 7:
         print("Invalid Entry")
     elif n == 1:
         menu1()
@@ -118,6 +168,10 @@ def userrequest(n):
         menu4()
     elif n == 5:
         menu5()
+    elif n == 6:
+        emergency()
+    elif n == 7:
+        nameattack()
     else:
         print("smiles....bug everywhere")
 
@@ -126,37 +180,56 @@ def userrequest(n):
 def menu1():
     print("Lemme check if he is around")
     # send a beep to my device to notify me about the visitor
-    #	notifybay(program in progress)
+    # notify bay(program in progress)
     time.sleep(5)
     print("Sorry..." + name.upper() + " he is not around")
+    baybot_message = name.upper() + " ask of you. I said you weren't around"
+    # saving message to baybotdb
+    cursor = db_conn.cursor()
+    add_baybot_entry = ("INSERT INTO visitor"
+                        "(visitor_name, message)"
+                        "VALUES(%s, %s)")
+    baybot_data_entry = (bot_name, baybot_message)
+    cursor.execute(add_baybot_entry, baybot_data_entry)
+    db_conn.commit()
+    cursor.close()
+    db_conn.close()
+
+    # saving message to notepad
+    offlinemessage = open("offlinemessage.txt", "a")
+    message_time = datetime.datetime.now()
+    sendmakeup = ("\n\n\n" + "Name: " + bot_name + "\n" + "Message: " +
+                  baybot_message + "\n" + "Time: " + str(message_time.strftime("%c")))
+    offlinemessage.write(sendmakeup)
+    offlinemessage.close()
+
     menu()
 
 
 # User menu
 def menu():
     animation(name)
-    print("")
+    # print("")
     time.sleep(0.2)
-    print(" \n1. I want to see " + developer.upper())
+    print("\n" + " 1. I want to see " + developer.upper() + "\n")
     time.sleep(0.2)
-    print("\n 2. I need BigBay's Number \n")
+    print(" 2. I need BigBay's Number" + "\n")
     time.sleep(0.2)
-    print(" 3. Put a call through to BigBay \n")
+    print(" 3. Put a call through to BigBay" + "\n")
     time.sleep(0.2)
-    print(" 4. I want to leave a message for him \n")
+    print(" 4. I want to leave a message for him" + "\n")
     time.sleep(0.2)
-    print(" 5. Do some mathematics \n")
+    print(" 5. Do some mathematics" + "\n")
     time.sleep(0.2)
-    print(" 6. Return")
-    usermenu = int(input(" Enter: "))
+    print(" 6. Emergency Number" + "\n")
+    print(" 7. Return")
+    usermenu = int(input("\nEnter: "))
     userrequest(usermenu)
 
 
 # admin menu
 def adminmenu():
-    Developer = "Ayodele Samuel Adebayo"
-    Developernickname = "UncleBigBay"
-    print("Welcome Programmer " + Developernickname.upper())
+    print("Welcome Programmer " + developernickname.upper())
     print(" ")
     print("1.Show Messages\n")
     print("2.number of visitors\n")
@@ -176,15 +249,15 @@ def detector(m):
 # check if name Boss or visitor
 def checkboss(n):
     db = "bigbay"
-    if name.lower() == db or name.lower() == "bayo":
+    if n.lower() == db or n.lower() == "bayo":
         print("\n Is that you boss? ")
         yesorno = input(" \n")
         if yesorno == "yes":
             print("\n Confirm Identity")
-            confirm = input(" \n")
-            detector(confirm)
+            confirm_identity = input(" \n")
+            detector(confirm_identity)
         else:
-            animation(name)
+            animation(n)
             menu()
     else:
         menu()
@@ -193,12 +266,14 @@ def checkboss(n):
 # Ask name of the visitor
 def nameattack():
     global name
-    print(" Hi, I am BayBot \n ")
+    print("\nHi, I am BayBot \n ")
     name = input("What is your name? ")
-
-    checkboss(name)
+    if name == "":
+        print("\nAnonymous not allowed..Anonymous is a name too")
+        nameattack()
+    else:
+        checkboss(name)
 
 
 # program start
 nameattack()
-
